0bc6d67b8e7f13fb8bc026eb4418b61cd3c77415
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-02-27 20:51:15 -0300

    General configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
+import React from 'react'
 import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Patinhas Bank</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main>
-        <h1>Patinhas Bank</h1>
-      </main>
-    </div>
+    </>
   )
 }
