a5fafa0e21af36b2e0a960741737810b7d64ad43
Rafael Pena <rafaelppena31@gmail.com>
2021-02-27 22:13:00 -0300

    Card

---
+++ src/components/shared/Card.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface CardProps {
+  title: string
+}
+
+export default function Card(): JSX.Element {
+  return (
+    <div>
+      <h1>oi</h1>
+    </div>
+  )
+}
