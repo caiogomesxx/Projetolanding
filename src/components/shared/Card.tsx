9e5e5724bad790c5aea1f8007259add4b9db614f
Rafael Pena <rafaelppena31@gmail.com>
2021-02-28 17:56:41 -0300

    Card with propTypes running

--- src/components/shared/Card.tsx
+++ src/components/shared/Card.tsx
@@ -1,20 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ReactCardFlip from 'react-card-flip'
 import styles from '../../styles/components/Card.module.css'
+
 interface CardProps {
   title: string
+  desc: string
+  beforeValue: number
+  actuallyValue: number
+  isProfile?: boolean
 }
 
-export default function Card(): JSX.Element {
+export default function Card({
+  actuallyValue,
+  beforeValue,
+  title,
+  desc
+}: CardProps): JSX.Element {
+  const [flip, setFlip] = useState(false)
+
+  const onCardClicked = e => {
+    e.preventDefault()
+    setFlip(!flip)
+  }
+
   return (
-    <div className={styles.cardContent}>
-      <div className={styles.textContent}>
-        <h1 className={styles.textName}>Day Trade</h1>
-        <p className={styles.textDesc}>Operações diárias alavancadas</p>
+    <ReactCardFlip
+      containerStyle={{
+        width: '100%',
+        maxWidth: '400px',
+        minHeight: '270px',
+        margin: '9px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '5px',
+        cursor: 'pointer'
+      }}
+      isFlipped={flip}
+    >
+      <div className={styles.cardContent} onClick={onCardClicked}>
+        <div className={styles.textContent}>
+          <h1 className={styles.textName}>{title}</h1>
+          <p className={styles.textDesc}>{desc}</p>
+        </div>
+        <div className={styles.valueContent}>
+          <p className={styles.textBeforeValue}>
+            {beforeValue.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </p>
+          <h1 className={styles.textActuallyValue}>
+            {actuallyValue.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </h1>
+        </div>
       </div>
-      <div className={styles.valueContent}>
-        <p className={styles.textBeforeValue}>R$ 0,00</p>
-        <h1 className={styles.textActuallyValue}>R$ 0,00</h1>
+      <div className={styles.cardContent} onClick={onCardClicked}>
+        <div className={styles.textContent}>
+          <h1 className={styles.textName}>{title}</h1>
+          <p className={styles.textDesc}>{desc}</p>
+        </div>
+        <div className={styles.valueContent}>
+          <p className={styles.textBeforeValue}>
+            {beforeValue.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </p>
+          <h1 className={styles.textActuallyValue}>
+            {actuallyValue.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </h1>
+        </div>
       </div>
-    </div>
+    </ReactCardFlip>
   )
 }
