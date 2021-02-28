cca54e611bc245e742820f042098c6898872b182
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-02-28 01:37:48 -0300

    Form and input mask added

--- src/components/section/FourthSection.tsx
+++ src/components/section/FourthSection.tsx
@@ -1,16 +1,107 @@
-import { Formik } from 'formik';
-import React from 'react';
+import React, { useState } from 'react';
+import { Formik, ErrorMessage } from 'formik';
+import MaskedInput from 'react-text-mask';
 
 import styles from '../../styles/Fourth.module.css';
 
-const ThirdSection: React.FC = () => {
-  return(
-    <div id={styles.fourth_section}>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quaerat!</h1>
-      
-      
-    </div>
+import { Button } from '../form/Button';
+import { Input } from '../form/Input';
+
+function FourthSection(): JSX.Element {
+  const phoneNumberMask = [
+    "(",
+    /\d/,
+    /\d/,
+    ")",
+    " ",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/,
+    "-",
+    /\d/,
+    /\d/,
+    /\d/,
+    /\d/
+  ];
+
+  return (
+    <section id={styles.fourth_section}>
+      <div>
+        <div className={styles.title_container}>
+          <h1>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nisi repudiandae! Quia,
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+        </h1>
+
+          <h3>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, nihil!
+        </h3>
+
+        </div>
+        <div className={styles.form_container}>
+          <Formik
+            onSubmit={(value) => {
+              const changedValue = value.telefone
+                .replace(/\)/g, '')
+                .replace(/\(/g, '')
+                .replace(/-/g, '')
+                .replace(/ /g, '');
+
+              alert('hoa')
+            }}
+            initialValues={{
+              name: '',
+              email: '',
+              telefone: '',
+              mensagem: '',
+            }}
+          >
+            {({ isSubmitting, values, setFieldValue }) => (
+              <form>
+                <div>
+                  <Input placeholder="Nome" />
+                  <ErrorMessage name="Nome" />
+                </div>
+                <div>
+                  <Input placeholder="Email" />
+                  <ErrorMessage name="Email" />
+                </div>
+                <div>
+                  <MaskedInput
+                    mask={phoneNumberMask}
+                    name="phone"
+                    onChange={e => {
+                      const value = e.target.value || '';
+                      console.log({ value });
+                      setFieldValue('phone', value);
+                    }}
+                    style={{
+                      padding: "12px 36px",
+                      margin: "5px",
+                      borderRadius: "5px",
+                      outline: "0",
+                      border: "0",
+                      textAlign: "center",
+                      fontSize: "15px",
+                      lineHeight: "16px",
+                    }}
+                    placeholder="Telefone"
+                  />
+                  <ErrorMessage name="telefone" />
+                </div>
+                <div>
+                  <textarea placeholder="mensagem" />
+                  <ErrorMessage name="mensagem" />
+                </div>
+                <Button disabled={isSubmitting}>Enviar</Button>
+              </form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </section>
   );
 }
 
-export { ThirdSection }
+export { FourthSection };
