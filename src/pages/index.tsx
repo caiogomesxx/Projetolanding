5591fbea1c0e95aab15144226845c274895bda71
Rafael Pena <rafaelppena31@gmail.com>
2021-02-28 23:51:32 -0300

    FirstSection Complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react'
 import Head from 'next/head'
+import React from 'react'
+import { FirstSection } from '../components/section/FirstSection'
+import { Header } from '../components/section/Header'
+import { SecondSection } from '../components/section/SecondSection'
 
-export default function Home(): JSX.Element {
+export default function Home() {
   return (
     <>
       <Head>
         <title>Patinhas Bank</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
+
+      <FirstSection />
+      <SecondSection />
     </>
   )
 }
