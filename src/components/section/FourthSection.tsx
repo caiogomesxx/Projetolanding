f100b5f4f77c8a8ccc9dd4140655367063fb77e7
Rafael Pena <rafaelppena31@gmail.com>
2021-03-02 11:12:51 -0300

    Lint configs

--- src/components/section/FourthSection.tsx
+++ src/components/section/FourthSection.tsx
@@ -1,16 +1,14 @@
-import { Formik } from 'formik';
-import React from 'react';
-
-import styles from '../../styles/Fourth.module.css';
+import React from 'react'
+import styles from '../../styles/Fourth.module.css'
 
 const ThirdSection: React.FC = () => {
-  return(
+  return (
     <div id={styles.fourth_section}>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quaerat!</h1>
-      
-      
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quaerat!
+      </h1>
     </div>
-  );
+  )
 }
 
 export { ThirdSection }
