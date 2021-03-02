746f6b07f8c71ddf7e06b1f368a6114a6525f6dc
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2021-03-01 21:55:51 -0300

    Changes to textarea and changed text

--- src/components/section/FourthSection.tsx
+++ src/components/section/FourthSection.tsx
@@ -28,68 +28,65 @@ function FourthSection(): JSX.Element {
 
   return (
     <section id={styles.fourth_section}>
-        <div className={styles.title_container}>
-          <h1>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid, nisi repudiandae! Quia,
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
-        </h1>
+      <div className={styles.title_container}>
+        <h1> Queremos te conhecer !! </h1>
 
-          <h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, nihil!
+        <h3>
+          Preencha o formulário abaixo para nossa empresa e deixe também um feedback do que achou do nosso site até agora!
         </h3>
 
-        </div>
-        <div className={styles.form_container}>
-          <Formik
-            onSubmit={() => console.log("Submited")}
-            initialValues={{
-              name: '',
-              email: '',
-              telefone: '',
-              mensagem: '',
-            }}
-          >
-            {({ isSubmitting, values, setFieldValue }) => (
-              <form>
-                <div>
-                  <Input placeholder="Nome" />
-                  <ErrorMessage name="Nome" />
-                </div>
-                <div>
-                  <Input placeholder="Email" />
-                  <ErrorMessage name="Email" />
-                </div>
-                <div>
-                  <MaskedInput
-                    mask={phoneNumberMask}
-                    name="phone"
-                    onChange={e => {
-                      const value = e.target.value || '';
-                      setFieldValue('phone', value);
-                    }}
-                    style={{
-                      padding: "12px 36px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      outline: "0",
-                      border: "0",
-                      textAlign: "center",
-                      fontSize: "15px",
-                      lineHeight: "16px",
-                    }}
-                    placeholder="Telefone"
-                  />
-                  <ErrorMessage name="telefone" />
-                </div>
-                <div>
-                  <textarea placeholder="Mensagem" />
-                  <ErrorMessage name="mensagem" />
-                </div>
-                <Button disabled={isSubmitting}>Enviar</Button>
-              </form>
-            )}
-          </Formik>
-        </div>
+      </div>
+      <div className={styles.form_container}>
+        <Formik
+          onSubmit={() => console.log("Submited")}
+          initialValues={{
+            name: '',
+            email: '',
+            telefone: '',
+            mensagem: '',
+          }}
+        >
+          {({ isSubmitting, values, setFieldValue }) => (
+            <form>
+              <div>
+                <Input placeholder="Nome" />
+                <ErrorMessage name="Nome" />
+              </div>
+              <div>
+                <Input placeholder="Email" />
+                <ErrorMessage name="Email" />
+              </div>
+              <div>
+                <MaskedInput
+                  mask={phoneNumberMask}
+                  name="phone"
+                  onChange={e => {
+                    const value = e.target.value || '';
+                    setFieldValue('phone', value);
+                  }}
+                  style={{
+                    padding: "12px 36px",
+                    margin: "5px",
+                    borderRadius: "5px",
+                    outline: "0",
+                    border: "0",
+                    textAlign: "center",
+                    fontSize: "15px",
+                    lineHeight: "16px",
+                  }}
+                  placeholder="Telefone"
+                />
+                <ErrorMessage name="telefone" />
+              </div>
+              <div>
+                <textarea placeholder="Mensagem" />
+                <ErrorMessage name="mensagem" />
+              </div>
+              <Button disabled={isSubmitting}>Enviar</Button>
+            </form>
+          )}
+        </Formik>
+      </div>
     </section>
   );
 }
