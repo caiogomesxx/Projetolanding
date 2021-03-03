26cf291e96cb8341063ac90ed1e6808306c3f62a
Rafael Pena <rafaelppena31@gmail.com>
2021-03-03 00:32:03 -0300

    Finish Second Section

--- src/components/section/SecondSection.tsx
+++ src/components/section/SecondSection.tsx
@@ -1,10 +1,56 @@
 import React from 'react'
+import stylesCards from '../../styles/Cards.module.css'
 import styles from '../../styles/Second.module.css'
+import { Graphc } from '../section/Graphc'
+import { Line } from '../section/Line'
+import { Cards } from './Cards'
 
-function SecondSection(): JSX.Element {
+const SecondSection: React.FC = () => {
   return (
-    <section className={styles.body}>
+    <section id={styles.secondSection}>
       <div className={styles.triangulo}></div>
+      <div id={styles.secondSectionTitle}>
+        <h1>Por que somos os melhores?</h1>
+        <h2>
+          Além de sermos a única investidora no mercado que abraça as causas
+          ecológicas, possuímos taxas imperdíveis
+        </h2>
+      </div>
+      <div className={styles.contentCards}>
+        <Cards white>
+          <div className={stylesCards.whiteTitle}>
+            <h1>SANB11</h1>
+            <p>$38,11</p>
+          </div>
+          <div className={stylesCards.acao}>
+            <h1>3.14%</h1>
+          </div>
+        </Cards>
+        <Cards white>
+          <div className={stylesCards.whiteTitle}>
+            <h1>VALE3</h1>
+            <p>$101,60</p>
+          </div>
+          <div className={stylesCards.acao}>
+            <h1>-3.07%</h1>
+          </div>
+        </Cards>
+        <Cards white>
+          <div className={stylesCards.whiteTitle}>
+            <h1>AZUL4</h1>
+            <p>$38,79</p>
+          </div>
+          <div className={stylesCards.red}>
+            <h1>-3.15%</h1>
+          </div>
+        </Cards>
+      </div>
+      <div className={styles.secondSectionMain}>
+        <div className={styles.secondSectionMain2}>
+          <Line />
+          <Graphc />
+        </div>
+      </div>
     </section>
   )
 }
