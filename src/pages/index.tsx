614df473c6bc21b4c413e3a640d5d83593f10c87
Rafael Pena <rafaelppena31@gmail.com>
2021-03-03 00:32:44 -0300

    Finish index structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 import { FirstSection } from '../components/section/FirstSection'
 import { FourthSection } from '../components/section/FourthSection'
 import { Header } from '../components/section/Header'
+import ProfileSection from '../components/section/ProfileSection'
 import { SecondSection } from '../components/section/SecondSection'
+import ThirdSection from '../components/section/ThirdSection'
 
 export default function Home() {
   return (
@@ -12,8 +14,11 @@ export default function Home() {
         <title>Patinhas Bank</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Header />
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
+      <ProfileSection />
       <FourthSection />
     </>
   )
