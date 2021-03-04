fc94e42b40484e8de9d00d9ead1d2840d9480185
Rafael Pena <rafaelppena31@gmail.com>
2021-03-03 23:33:09 -0300

    Path

--- src/components/graphic/Line.tsx
+++ src/components/graphic/Line.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import style from '../../styles/components/graphic/line.module.css'
+import style from '../../styles/components/graphic/Line.module.css'
 
 const Line: React.FC = () => {
   return (
