3d37d49915467b6e5fb04c66aae9e1bd1b06fa7c
Rafael Pena <rafaelppena31@gmail.com>
2021-03-03 23:23:22 -0300

    type

--- src/components/form/Button.tsx
+++ src/components/form/Button.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styles from '../../styles/components/forms/Button.module.css'
 
 interface IButtonProps {
+  children: ReactNode
   disabled: boolean
 }
 
-const Button: React.FC = ({ children }, { disabled }: IButtonProps) => {
+const Button: React.FC<IButtonProps> = ({ children, disabled }) => {
   return (
     <button disabled={disabled} className={styles.button}>
       {children}
